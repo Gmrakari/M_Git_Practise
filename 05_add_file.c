e78ce24fe507d4cb1ddc9366dd07350be9a923d8
拉卡里大海 <gmrakari@outlook.com>
2022-04-28 08:58:38 +0800

    [feature]rebase_commit_practise

--- 05_add_file.c
+++ 05_add_file.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
+    int a = 10;
+    if (a < 5) {
+        printf("a < 5\n");
+    }
+    else {
+        printf("a > 5\n");
+    }
     printf("05_add_file.c");
+    printf("i need to rebase the top commit into one line\n");
+
     return 0;
 }
